477bb581097b351500c7b4fbb5c77eb79ee950cb
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2023-11-07 07:31:20 -0400

    component modifications

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,47 @@
 import '../assets/styles/header.css'
-import {AppBar, Typography, Toolbar} from '@mui/material';
+import {AppBar, Typography, Toolbar, Button} from '@mui/material';
 import { TiShoppingCart } from 'react-icons/ti';
 import { BiUserCircle } from 'react-icons/bi';
 
 function Header(){
     return (
-        <AppBar className="header" >
-      <header className="left-header">
-        <Toolbar>
-        <Typography>The Liko</Typography>
-        </Toolbar> 
-      </header>
-      <header className="center-header">
-          <ul>
-            <li>Liqueurs</li>
-            <li>Soft Drinks</li>
-            <li>Extras</li>
-          </ul>
-      </header>
-        <header className="right-header">
-          <ul>
-            <TiShoppingCart />
-            <BiUserCircle />
-          </ul>
-      </header>
-    </AppBar>
+        <AppBar className="header" sx={{background:'#FFF'}}>
+            <Toolbar sx={{color:'#FF0000'}}>
+                <header className="left-header">
+                <Typography sx={{color:'#FF0000'}}>The Liko</Typography> 
+                </header>
+                <header className="center-header">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/">Liquors</a>
+                        </li>
+                        <li>
+                            <a href="/">Soft Drinks</a>
+                        </li>
+                        <li>
+                            <a href="/">Extras</a>
+                        </li>
+                        <li>
+                            <a href="/">About Us</a>
+                        </li>
+                        <li>
+                            <input placeholder='Search...'></input>
+                        </li>
+                    </ul>
+                </header>
+                <header className="right-header">
+                    <ul>
+                        <a to="/">Sing In</a>
+                        <span> | </span>
+                        <a to="/">Create Account</a>
+                        <TiShoppingCart />
+                    </ul>
+                </header>
+            </Toolbar>
+        </AppBar>
     )
 }
 
