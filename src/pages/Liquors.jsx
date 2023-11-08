72095d5baf2c123c6301faa73bd5804500834700
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2023-11-08 10:35:42 -0400

    corrections

--- src/pages/Liquors.jsx
+++ src/pages/Liquors.jsx
@@ -1,5 +1,5 @@
-import Header from '../components/header';
-import CommingSoon from '../components/comingSoon';
+import Header from '../components/Header';
+import CommingSoon from '../components/ComingSoon';
 
 
 const Liquors = () => {
