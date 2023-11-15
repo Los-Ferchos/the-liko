67989c4eb6d944c552899e6e7aa7c48da7b81f61
Fernando Mauricio Mamani Navarro <fernando.mamani.13389201@usalesiana.edu.bo>
2023-11-15 16:31:47 -0400

    feat:form checkout

--- src/components/checkout/CheckoutForm.jsx
+++ src/components/checkout/CheckoutForm.jsx
@@ -1,10 +1,81 @@
-import {PaymentElement} from '@stripe/react-stripe-js';
+import {PaymentElement, CardElement} from '@stripe/react-stripe-js';
+import {TextField, Typography, Tab, Box} from '@mui/material';
+import { useState } from 'react';
+import '../../assets/styles/checkout.css'
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 const CheckoutForm = () => {
+  const [FirstName, setFirstName] = useState('');
+  const [LastName, setLastName] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [deliveryAddress, setDeliveryAddress] = useState('');
+  const {nit, setNit} = useState('');
+
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  
+  
   return (
-    <form>
-      <PaymentElement />
-      <button>Submit</button>
+    <form className='checkout-form'>
+      <Typography variant='h6' >1. DELIBERY ADDRESS</Typography>
+      <TextField
+      label="First Name"
+      size='normal' 
+      variant="outlined"
+      onChange={e => setFirstName(e.target.value)}
+      />
+
+      <TextField
+      label="Last Name"
+      size='normal' 
+      variant="outlined"
+      onChange={e => setLastName(e.target.value)}
+      />
+
+      <TextField
+      label="Telephone"
+      size='normal' 
+      variant="outlined"
+      onChange={e => setTelephone(e.target.value)}
+      />
+
+      <TextField
+      required
+      label="Delivery Address"
+      size='normal' 
+      variant="outlined"
+      onChange={e => setDeliveryAddress(e.target.value)}
+      />
+
+
+      <TextField
+      label="NIT"
+      size='normal' 
+      variant="outlined"
+      onChange={e => setNit(e.target.value)}
+      />
+      <div className='payment-method'>
+         <Typography variant='h6' >2. PAYMENT METHOD</Typography>
+          <Box sx={{ width: '100%', typography: 'body1' }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={handleChange} aria-label="lab API tabs example">
+                <Tab label="Credit Card" value="1" />
+                <Tab label="QR" value="2" />
+              </TabList>
+            </Box>
+            <TabPanel value="1"><PaymentElement/></TabPanel>
+            <TabPanel value="2">Item Two</TabPanel>
+          </TabContext>
+          </Box>
+      </div>
+     
     </form>
   );
 };
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { Container } from '@mui/material'
-import Header from "../components/Header"
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, TextField } from '@mui/material';
+import Header from '../components/Header';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from '../components/checkout/CheckoutForm';
-import { API_URL_LINK } from "../utils/constants";
+import { API_URL_LINK, STRIPE_KEY } from '../utils/constants';
+import '../assets/styles/checkout.css'
+import '../assets/styles/index.css'
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_51OCBE7IJLm2DQzDdDIUITH16K4zwb5rrxlYjGN4eLieGLIB9E9AaruQQSzYQZbOjAiiHK5nhITmWMqoSByOWynkw006GJSkl1b');
+const stripePromise = loadStripe(STRIPE_KEY);
 
-const Checkout = ({amount, currency}) => {
-
-  const [clientSecret, setClientSecret] = useState('pi_3OCG3IIJLm2DQzDd1VLwDQiJ_secret_gkx2dDgsbmWT5DC36RqiNFyUC');
+const Checkout = () => {
+  const [clientSecret, setClientSecret] = useState('');
+  const amount = 100;
+  const currency = 'usd';
 
   useEffect(() => {
     const fetchClientSecret = async () => {
-      
       try {
         const response = await fetch(`${API_URL_LINK}/codeCheckout`, {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
           },
-          body: JSON.stringify({"amount":100, "currency":"usd"})
+          body: JSON.stringify({ amount, currency }),
         });
         if (!response.ok) {
           throw new Error('Error al obtener el clientSecret');
@@ -31,24 +31,38 @@ const Checkout = ({amount, currency}) => {
 
         const data = await response.json();
         setClientSecret(data.clientSecret);
-        console.log(data.clientSecret);
+        console.log('clientSecret después de setClientSecret:', data.clientSecret);
       } catch (error) {
-        console.error('Error al obtener el clientSecret:', error);
+        console.log('Error al obtener el clientSecret:', error);
       }
     };
 
-    fetchClientSecret();
-  }, []);
-    
+    const fetchData = async () => {
+      await fetchClientSecret();
+    };
+
+    fetchData();
+  }, [amount, currency, setClientSecret]); 
+
   return (
     <Container>
-        <Header/>
-        <h1>Checkout</h1>
-        <Elements stripe={stripePromise} options={{clientSecret}}>
-          <CheckoutForm />
-        </Elements>
+      <Header />
+      {clientSecret && (
+        <div className='container-form'>
+          <div className='form'>
+            <Typography variant='h4'style={{ fontWeight: "bold"}} >Billing  Details</Typography>
+            <Elements stripe={stripePromise} options={{ clientSecret }}>
+              <CheckoutForm />
+             </Elements>
+          </div>
+          <div>
+            <h1>Hola-----------------------------------------------____________________________________________</h1>
+          </div>
+          
+        </div>
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
