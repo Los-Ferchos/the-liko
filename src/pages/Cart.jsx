b25954314de722e6f5c2f84c513c20afc75fc717
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2023-11-20 16:59:15 -0400

    responsive

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,10 +8,9 @@ import { useNavigate } from 'react-router-dom';
 import { useGlobalCart } from '../components/contexts/CartContext'
 
 function Cart() {
-    const [subtotal, setSubtotal] = useState(0);
     const navigate = useNavigate();
     const cart = useGlobalCart();
-    const [tax, setTax] = useState(5.3);
+    const [tax, setTax] = useState(0);
 
     const getSubtotalProduct = (product) => {
         return (product.quantity * product.productInfo.price.value)
@@ -19,12 +18,14 @@ function Cart() {
 
     const getTotal = () => {
         var total = 0;
-        {cart.cartItems.map((product) => (
-            total+=getSubtotalProduct(product)
-        ))}
+        {
+            cart.cartItems.map((product) => (
+                total += getSubtotalProduct(product)
+            ))
+        }
         return total
     }
-    
+
     return (
         <Container component={"section"} style={{ position: "relative" }}>
             <Header />
@@ -33,45 +34,69 @@ function Cart() {
                 activePath='Cart'
             />
             <Typography variant='h4' color='primary' component='h1' marginTop={8} marginBottom={8}>Shopping Cart</Typography>
-            <Grid container spacing={30}>
-                <Grid item className='products'>
-                    <div style={{ padding: 20 }}>
-                            {cart.cartItems.map((product, index) => (
-                                <CartProduct
-                                    cart={cart}
-                                    product={product}
-                                    key={index}
-                                />
-                            ))}
-                        <Button variant='outlined' style={{marginTop:10}}onClick={() => {
-                                navigate("/products")
-                            }}>Continue Shopping</Button>
+            {
+                cart.cartItems.length > 0 ?
+                    <div >
+                        <div className='container'>
+                            <div className='products'>
+                                <div style={{ padding: 20 }}>
+                                    <div className='title-details'>
+                                        <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6} width={"27%"} justifyContent={"center"}>Product</Typography>
+                                        <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6} width={"12%"}>Price</Typography>
+                                        <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6} width={"18%"}>Quantity</Typography>
+                                        <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6} width={"18%"}>Subtotal</Typography>
+                                        <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6} width={"12%"}>Trash</Typography>
+
+                                    </div>
+                                    {cart.cartItems.map((product, index) => (
+                                        <CartProduct
+                                            cart={cart}
+                                            product={product}
+                                            key={index}
+                                        />
+                                    ))}
+                                    <Button variant='outlined' style={{ marginTop: 10 }} onClick={() => {
+                                        navigate("/products")
+                                    }}>Continue Shopping</Button>
+                                </div>
+                            </div>
+                            <div className='order-summary' style={{ position: "sticky", top: "20px" }}>
+                                <Card style={{ padding: 20, alignContent: "center", alignItems: "center" }}>
+                                    <div>
+                                        <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6}>Order Summary</Typography>
+                                    </div>
+                                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Typography variant='h6' color='black' component='h1'>Subtotal</Typography>
+                                        <Typography variant='h6' color='black' component='h1'>{getTotal()}</Typography>
+                                    </div>
+                                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Typography variant='h6' color='black' component='h1'>Tax</Typography>
+                                        <Typography variant='h6' color='black' component='h1'>{tax}%</Typography>
+                                    </div>
+                                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Typography variant='h6' color='black' component='h1'>Total</Typography>
+                                        <Typography variant='h6' color='black' component='h1'>{((getTotal()) + (getTotal() * tax / 100)).toFixed(2)}</Typography>
+                                    </div>
+                                    <div style={{ display: "flex", justifyContent: "center" }}>
+                                        <Button variant='outlined' style={{ marginTop: 10, alignContent: "center" }}
+                                            onClick={() => {
+                                                navigate("/checkout")
+                                            }}>Checkout</Button>
+                                    </div>
+
+                                </Card>
+                            </div>
+                        </div>
                     </div>
-                </Grid>
-                <Grid item className='order-summary' style={{ position: "sticky", top: "20px" }}>
-                    <Card style={{ padding: 20 }}>
-                        <Grid item>
-                            <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6}>Order Summary</Typography>
-                        </Grid>
-                        <Grid item style={{ display: "flex", justifyContent: "space-between" }}>
-                            <Typography variant='h6' color='black' component='h1'>Subtotal</Typography>
-                            <Typography variant='h6' color='black' component='h1'>{getTotal()}</Typography>
-                        </Grid>
-                        <Grid item style={{ display: "flex", justifyContent: "space-between" }}>
-                            <Typography variant='h6' color='black' component='h1'>Tax</Typography>
-                            <Typography variant='h6' color='black' component='h1'>{tax}%</Typography>
-                        </Grid>
-                        <Grid item style={{ display: "flex", justifyContent: "space-between" }}>
-                            <Typography variant='h6' color='black' component='h1'>Total</Typography>
-                            <Typography variant='h6' color='black' component='h1'>{((getTotal())+(getTotal()*tax/100)).toFixed(2)}</Typography>
-                        </Grid>
-                        <Button variant='outlined' style={{marginTop:10, alignContent:"center"}}
-                            onClick={() => {
-                                navigate("/products")
-                            }}>Checkout</Button>
-                    </Card>
-                </Grid>
-            </Grid>
+                    :
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-between", height: "12vh", padding: "150px" }}>
+                        <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6}>No Items in Cart</Typography>
+                        <Button variant='outlined' style={{ marginTop: 10 }} onClick={() => {
+                            navigate("/products")
+                        }}>Continue Shopping</Button>
+                    </div>
+            }
+
         </Container>
     )
 }
