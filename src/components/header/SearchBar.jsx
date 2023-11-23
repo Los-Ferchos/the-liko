b2073897b90b911532d25cb466285c0d6f5a7dab
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2023-11-23 05:19:26 -0400

    Feat: implement JSDocs

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -4,6 +4,19 @@ import { TextField, InputAdornment, IconButton } from '@mui/material';
 import { FaSistrix } from 'react-icons/fa';
 import '../../assets/styles/search.css';
 
+/**
+ * @typedef {Object} SearchBarProps
+ * @property {string} searchText - El texto actual del campo de búsqueda.
+ * @property {function} handleChangeSearch - Función para manejar cambios en el texto de búsqueda.
+ * @property {function} onKeyPress - Función para manejar eventos de pulsación de tecla.
+ * @property {function} onButtonClick - Función para manejar eventos de clic en el botón de búsqueda.
+ */
+
+/**
+ * Componente de barra de búsqueda.
+ * @param {SearchBarProps} props - Propiedades del componente.
+ * @returns {JSX.Element} Elemento JSX renderizado del componente.
+ */
 const SearchBar = ({
   searchText,
   handleChangeSearch,
