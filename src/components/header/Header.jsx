a40beea61266e300443e6cce588dd91c11786420
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2023-11-23 04:59:45 -0400

    Feat: Finally seearch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,45 +15,49 @@ import { useAppSelector } from '../hooks/store';
  */
 const Header = () => {
     const { width } = useWindowSize();
-    const limit = 960;
+    const limit = 990;
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const [isSearchVisible, setIsSearchVisible] = useState(false); // Agrega el estado para la visibilidad
+    const [isSearchVisible, setIsSearchVisible] = useState(false);
     const searchText = useAppSelector((state) => state.search.searchText);
-
-
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-          navigate("/products");
-          dispatch(setSearch());
-        }
-      };
   
-
-
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        navigate("/products");
+        dispatch(setSearch());
+      }
+    };
+  
     const handleChangeSearch = (e) => {
-        dispatch(setSearchText(e.target.value))
-    }
-
+      dispatch(setSearchText(e.target.value))
+    };
+  
+    const handleSearchButtonClick = () => {
+      navigate("/products");
+      dispatch(setSearch());
+    };
+  
     return (
-        <AppBar color=''>
-            <Container style={{ paddingTop: 0, paddingBottom: '10px' }}>
-                {
-                    width > limit
-                    ? <NormalHeader  isSearchVisible={isSearchVisible} setIsSearchVisible={setIsSearchVisible}/>
-                    : <SmallHeader/>
-                }
-                {isSearchVisible && ( // Renderiza el SearchBar solo si isSearchVisible es true
-                    <SearchBar
-                        searchText={searchText}
-                        handleChangeSearch={handleChangeSearch}
-                        onKeyPress={handleKeyPress}
-                        className='search'
-                    />
-                )}
-            </Container>
-        </AppBar>
-    )
-}
-
-export default Header
+      <AppBar color=''>
+        <Container style={{ paddingTop: 0, paddingBottom: '10px' }}>
+          {width > limit ? (
+            <NormalHeader isSearchVisible={isSearchVisible} setIsSearchVisible={setIsSearchVisible}/>
+          ) : (
+            <SmallHeader isSearchVisible={isSearchVisible} setIsSearchVisible={setIsSearchVisible}/>
+          )}
+          {isSearchVisible && (
+            <SearchBar
+              searchText={searchText}
+              handleChangeSearch={handleChangeSearch}
+              onKeyPress={handleKeyPress}
+              onButtonClick={handleSearchButtonClick} 
+              isSearchClicked={isSearchVisible} 
+              className='search'
+            />
+          )}
+        </Container>
+      </AppBar>
+    );
+  };
+  
+  export default Header;
--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,33 +1,36 @@
+// SearchBar.jsx
 import React from 'react';
 import { TextField, InputAdornment, IconButton } from '@mui/material';
 import { FaSistrix } from 'react-icons/fa';
-import '../../assets/styles/search.css'
+import '../../assets/styles/search.css';
 
 const SearchBar = ({
   searchText,
   handleChangeSearch,
   onKeyPress,
+  onButtonClick,
 }) => {
   return (
-    <div >
-        <TextField 
-          size='small'
-          placeholder='Search'
-          value={searchText}
-          onChange={handleChangeSearch}
-          fullWidth
-          autoComplete='off'
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton edge="end" onClick={onKeyPress}>
-                  <FaSistrix />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-          onKeyDown={onKeyPress}
-        />
+    <div style={{ justifyContent: 'center', display: 'flex', alignItems: 'center' }} >
+      <TextField
+        size='small'
+        placeholder='Search'
+        value={searchText}
+        onChange={handleChangeSearch}
+        fullWidth
+        autoComplete='off'
+        sx={{ width: '850px', paddingRight: '5px' }} 
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton edge="end" onClick={onButtonClick} >
+                <FaSistrix />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        onKeyDown={onKeyPress}
+      />
     </div>
   );
 };
