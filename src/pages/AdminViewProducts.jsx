9214230b34125c06a976b4347b055182abea55fd
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2023-11-23 02:27:28 -0400

    fab button added

--- src/pages/AdminViewProducts.jsx
+++ src/pages/AdminViewProducts.jsx
@@ -1,30 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Typography, CircularProgress } from '@mui/material';
-import CommingSoon from '../components/ComingSoon';
+import { Container, Typography, CircularProgress, Fab } from '@mui/material';
 import Header from '../components/header/Header';
 import NavigationText from '../components/navText/NavigationText';
 import { useNavigate, useParams } from 'react-router-dom';
-import { capitalizeString, filterDataArray, getInactiveAdminPaths } from '../utils/methods';
-import SubcategoriesList from '../components/categories/SubcategoriesList';
+import { capitalizeString, getInactiveAdminPaths } from '../utils/methods';
 import ProductsDisplay from '../components/products/ProductsDisplay';
 import { API_URL_LINK } from '../utils/constants';
+import AddIcon from '@mui/icons-material/Add';
+import useWindowSize from '../components/hooks/useWindowSize';
 
-const AdminViewProducts = ({destination = ""}) => {
+const AdminViewProducts = ({ }) => {
   const { name = "Products", nameCat = "" } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const actualUrl = `${API_URL_LINK}/products`;
-
+  const { width } = useWindowSize();
 
   return (
     <Container>
-        <Header/>
-        <>
-          <NavigationText inactivePath={getInactiveAdminPaths(nameCat)} activePath={capitalizeString(name)} />
-          <Typography variant='h4' color='primary' component='h1' marginTop={6}>
-            {capitalizeString(name)}
-          </Typography>
-          <ProductsDisplay apiUrl={actualUrl} loading={false} type={"admin"} />
-        </>
+      <Header />
+      <>
+        <NavigationText inactivePath={getInactiveAdminPaths(nameCat)} activePath={capitalizeString(name)} />
+        <Typography variant='h4' color='primary' component='h1' marginTop={6}>
+          {capitalizeString(name)}
+        </Typography>
+        <ProductsDisplay apiUrl={actualUrl} loading={false} type={"admin"} />
+        <Fab
+          style={{ position: "fixed", bottom: 40, right: 40, zIndex: 1000 }}
+          color="primary"
+          size={width<960?'medium':''}>
+          <AddIcon />
+        </Fab>
+      </>
     </Container>
   )
 }
