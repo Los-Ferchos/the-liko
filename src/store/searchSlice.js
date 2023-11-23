62d3df8372346badedffb96fcfed23dc58b78f57
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2023-11-23 05:20:49 -0400

    Feat: implement JSDocs

--- src/store/searchSlice.js
+++ src/store/searchSlice.js
@@ -1,5 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+/**
+ * Redux slice for search-related state.
+ *
+ * @typedef {Object} SearchState
+ * @property {string} searchText - The current search text.
+ * @property {boolean} search - Flag indicating whether a search is active or not.
+ */
+
+/**
+ * Redux slice for search-related state.
+ *
+ * @type {import("@reduxjs/toolkit").Slice<SearchState>}
+ */
 export const searchSlice = createSlice({
     name: "search",
     initialState: {
