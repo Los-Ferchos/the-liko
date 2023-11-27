bf549d46520e20efe8fe97e802372ee9235d3354
Fernando Mauricio Mamani Navarro <fernando.mamani.13389201@usalesiana.edu.bo>
2023-11-27 08:58:47 -0400

    fix: resolve merge conflicts

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 import { Container, Typography } from '@mui/material';
 import Header from '../components/header/Header';
 import useWindowSize from '../components/hooks/useWindowSize';
