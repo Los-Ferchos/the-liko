ce460343258fc032b9d899095dcc731ba9b8c22c
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2023-11-27 12:15:16 -0400

    details section and responsive

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -9,23 +9,37 @@ import LazyImage from '../components/images/LazyImage'
 import bottleLoaderImg from '../assets/images/bottle-loader.png'
 import { Typography } from '@mui/material'
 import RatingProduct from '../components/products/rating/RatingProduct'
+import '../assets/styles/productDetails.css'
+import { useAppSelector } from '../components/hooks/store'
 
 function ProductDetails() {
   const location = useLocation();
   const product = location.state.product;
+  const categories = useAppSelector((state) => state.categories.categories);
+  const subcategories = useAppSelector((state) => state.subcategories.subcategories);
 
-  console.log(product)
+  const getCategory = () => {
+    const foundCategory = categories.find(category => category._id === product.category);
+    return foundCategory ? foundCategory.name : '';
+  };
+
+  const getSubcategory = () => {
+    const foundSubcategory = subcategories.find(subcategory => subcategory._id === product.subcategory);
+    return foundSubcategory ? foundSubcategory.name : '';
+  };
+
+
   return (
     <>
-      <Container component={"section"}>
+      <Container component={"section"} style={{ position: "relative" }}>
         <Header />
         <NavigationText
           inactivePath={[{ title: "Home", href: "/" }, { title: "Products", href: `/products` }]}
           activePath={product.name}
         />
         <div>
-          <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
-            <div>
+          <div className={'information'} >
+            <div className='image'>
               <LazyImage
                 src={product.imgUrl}
                 placeholderSrc={bottleLoaderImg}
@@ -33,7 +47,7 @@ function ProductDetails() {
                 style={{ width: 350, height: 350 }}
               />
             </div>
-            <div style={{ width: '50%' }}>
+            <div className='product-info'>
               <Typography variant='h4' fontWeight="bold">{product.name}</Typography>
               <RatingProduct
                 rating={product.rating}
@@ -42,19 +56,48 @@ function ProductDetails() {
               <Typography variant='h6'>{`${product.price.value} ${product.price.currency}`}</Typography>
               <hr />
               <Typography variant='h6' fontWeight="bold">Product Descirption</Typography>
-              <Typography >{product.description}</Typography>
+              <Typography textAlign={'left'}>{product.description}</Typography>
             </div>
           </div>
-          <div>
+          <div className='detail-list' >
             <Typography variant='h6' color={'primary'} fontWeight="bold">Product Details</Typography>
-            <div>
-              <Typography fontWeight="bold">Category</Typography>
-              <Typography >{product.category}</Typography>
+            <hr />
+            <div style={{ display: 'flex', width: '100%' }}>
+              <div className='detail'>
+                <Typography fontWeight="bold">Category</Typography>
+              </div>
+              <div className='detail'>
+                <Typography >{getCategory()}</Typography>
+              </div>
             </div>
-            <div>
-              <Typography fontWeight="bold">Subcategory</Typography>
-              <Typography >{product.subcategory}</Typography>
+            <hr style={{ display: product.details.abv ? 'flex' : 'none' }} />
+            <div style={{ display: product.details.abv ? 'flex' : 'none', width: '100%' }}>
+              <div className='detail'>
+                <Typography fontWeight="bold">Subcategory</Typography>
+              </div>
+              <div className='detail'>
+                <Typography >{getSubcategory()}</Typography>
+              </div>
             </div>
+            <hr />
+            <div style={{ display: 'flex', width: '100%' }}>
+              <div className='detail'>
+                <Typography fontWeight="bold">Brand</Typography>
+              </div>
+              <div className='detail'>
+                <Typography >{product.details.brand}</Typography>
+              </div>
+            </div>
+            <hr style={{ display: product.details.abv ? 'flex' : 'none' }} />
+            <div style={{ display: product.details.abv ? 'flex' : 'none', width: '100%' }}>
+              <div className='detail'>
+                <Typography fontWeight="bold">ABV</Typography>
+              </div>
+              <div className='detail'>
+                <Typography >{product.details.abv}</Typography>
+              </div>
+            </div>
+            <hr />
           </div>
         </div>
 
