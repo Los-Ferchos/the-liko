7e6f10d9762b1fdff8435e7394e55658df6b1979
VillalobosGarciaGaby <gabriela.garcia.13355797@usalesiana.edu.bo>
2023-11-30 16:59:54 -0400

    Feature:Implement HomePage the imagen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,6 @@ import { API_URL_LINK } from '../utils/constants';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import ProductCarousel from '../components/products/carousel/ProductCarousel';
-import ProductCarouselItem from '../components/products/carousel/ProductCarouselItem';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
