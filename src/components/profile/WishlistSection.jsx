148e43ce67017dd85b0005b93a6469c12d5bf3d8
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2023-12-01 00:58:59 -0400

    no items in wishlist

--- src/components/profile/WishlistSection.jsx
+++ src/components/profile/WishlistSection.jsx
@@ -3,12 +3,15 @@ import { useGlobalCart } from '../contexts/CartContext';
 import { API_URL_LINK } from '../../utils/constants';
 import { useState, useEffect } from 'react';
 import WishItem from '../products/wishlist/WishItem';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
 
 const WishlistSection = () => {
   const [loading, setLoading] = useState(true);
   const { userLogged } = useGlobalCart();
   const [wishlist, setWishlist] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchWishlist = async () => {
@@ -30,16 +33,25 @@ const WishlistSection = () => {
   return (
     <>
       {
-        loading ? <div className="full-centered-container"><span className="loader"></span></div> : (
-          <div style={{width:'90%'}}>
-            <Typography variant="h5" color={'primary'} fontWeight={'bold'} marginBottom={10}>
+        loading ? (<div className="full-centered-container"><span className="fast-loader"></span></div>):
+        (!wishlist || wishlist === undefined || wishlist.lenght === 0) ?
+          <div className='no-items'>
+            <Typography variant='h5' color='black' component='h1' style={{ fontWeight: "bold" }} marginBottom={6}>No Items in Wishlist</Typography>
+            <Button variant='outlined' style={{ marginTop: 10 }} onClick={() => {
+              navigate("/products")
+            }}>Continue Navigating</Button>
+          </div>
+          :
+          (
+            <div style={{ width: '90%' }}>
+              <Typography variant="h5" color={'primary'} fontWeight={'bold'} marginBottom={10}>
                 Wishlist
               </Typography>
-            {wishlist.map((item) => (
-              <WishItem key={item.productId} productId={item.productId}/>
-            ))}
-          </div>
-        )
+              {wishlist.map((item) => (
+                <WishItem key={item.productId} productId={item.productId} />
+              ))}
+            </div>
+          )
       }
     </>
   )
